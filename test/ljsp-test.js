148f5433fee0579f760cf311723e5b70ffa36a98
Kenichi Kamiya <kachick1@gmail.com>
2017-01-09 22:47:59 +0900

    wip

--- test/ljsp-test.js
+++ test/ljsp-test.js
@@ -4,15 +4,24 @@ const expect = require("chai").expect
 const Ljsp = require("../lib/ljsp")
 
 describe('Ljsp', function() {
+  const tokens = ["(", "begin", "(", "define", "r", "3", ")", "(", "*", "3.141592653", "(", "*", "r", "r", ")", ")", ")"]
+  const parsedAtoms = ['begin', ['define', 'r', 3], ['*', 3.141592653, ['*', 'r', 'r']]]
+
   describe('.tokenize()', function() {
     it('', function() {
-      expect(Ljsp.tokenize("(begin (define r 3) (* 3.141592653 (* r r)))")).to.eql(["(", "begin", "(", "define", "r", "3", ")", "(", "*", "3.141592653", "(", "*", "r", "r", ")", ")", ")"])
+      expect(Ljsp.tokenize("(begin (define r 3) (* 3.141592653 (* r r)))")).to.eql(tokens)
     })
   })
 
   describe('.parse()', function() {
     it('', function() {
-      expect(Ljsp.parse("(begin (define r 3) (* 3.141592653 (* r r)))")).to.eql(['begin', ['define', 'r', 3], ['*', 3.141592653, ['*', 'r', 'r']]])
+      expect(Ljsp.parse("(begin (define r 3) (* 3.141592653 (* r r)))")).to.eql(parsedAtoms)
+    })
+  })
+
+  describe('.evaluate()', function() {
+    it('', function() {
+      expect(Ljsp.evaluate(parsedAtoms)).to.eql(28.274333877)
     })
   })
 })
